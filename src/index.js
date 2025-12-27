0870f03078311f70fae17f4342cf7a0e990e9bed
elinmolindr <elinmolinder1@gmail.com>
2025-12-27 16:41:21 +0100

    Add register imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './sections/main.scss';
 import './scripts/menu.js';
-
+import './scripts/register.js';
 import { toggleMenu } from './scripts/menu.js'; 
+import { toggleRegister } from './scripts/register.js';
 
 toggleMenu(); 
+toggleRegister();
 
