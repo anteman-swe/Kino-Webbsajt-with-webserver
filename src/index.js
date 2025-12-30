353808bf57ff3ad9e385cd671412e6d5bf94b98d
Tomas Panyokaew <tomaspanyokaew@hotmail.com>
2025-12-30 13:28:35 +0100

    adjust layout properties in main.scss

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ import './scripts/register.js';
 
 import { toggleLogin } from './scripts/login.js';
 toggleLogin();
-
 import { toggleRegister } from './scripts/register.js';
 toggleRegister();
-
 import { toggleMenu } from './scripts/menu.js'; 
 toggleMenu(); 
 import { closeNotice } from './scripts/notice.js';
