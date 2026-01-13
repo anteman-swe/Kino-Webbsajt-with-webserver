9996dc11adc6869c735bed1fe83d0c64407b1e43
Anders Eriksson <anteman.swe@gmail.com>
2026-01-13 10:20:33 +0100

    Added Nodemon to project

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+const port = 5080;
+
+app.use('/', express.static('./'));
+
+// app.get(' ', (req,  res) => {
+//     console.log(req.path);
+//     res.statusCode = 200;
+//     res.send('Requested path: '+ req.path);
+//     res.end();
+// });
+
+app.listen(port);
