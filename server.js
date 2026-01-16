0492bb3aa2a1081e923e6a6f6995f455f91bd997
Anders Eriksson <anteman.swe@gmail.com>
2026-01-16 10:26:48 +0100

    Restart of server.js

--- server.js
+++ server.js
@@ -4,13 +4,27 @@ const app = express();
 
 const port = 5080;
 
-app.use('/', express.static('./dist'));
+// app.use('/', express.static('./dist'));
 
-// app.get(' ', (req,  res) => {
-//     console.log(req.path);
-//     res.statusCode = 200;
-//     res.send('Requested path: '+ req.path);
-//     res.end();
-// });
+app.get('/', (req, res) =>{
 
-app.listen(port);
+});
+app.get('/index.html', (req, res) =>{
+
+});
+app.get('/', (req, res) =>{
+
+});
+app.get('/', (req, res) =>{
+
+});
+app.get('/', (req, res) =>{
+
+});
+app.get('/', (req, res) =>{
+
+});
+
+app.listen(port, () => {
+    console.log(`Server listening to port: ${port}`)
+});
