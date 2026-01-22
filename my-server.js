4c4b513e49ce52d0ace89c7e56513912df9977b7
Anders Eriksson <anteman.swe@gmail.com>
2026-01-22 18:33:07 +0100

    Added prettier plugin. Changed to partials folder in views for templates. Updated templates to fit.

--- my-server.js
+++ my-server.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import ejs from "ejs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -13,42 +13,48 @@ export default function initServer(api) {
   server.set("view engine", "ejs");
   server.set("views", path.join(__dirname, "views"));
 
-  server.get(['/','/index','/index.html'], (req, res) => {
-    res.render('index');
+  server.get(["/", "/index", "/index.html"], (req, res) => {
+    res.render("index");
   });
 
-  server.get(['/member-page','/memberpage','/member-page.html'], (req, res) => {
-    res.render('member-page');
-  });
+  server.get(
+    ["/member-page", "/memberpage", "/member-page.html"],
+    (req, res) => {
+      res.render("member-page");
+    },
+  );
 
-  server.get(['/breakfast-movie','/breakfastmovie','/breakfastmovie.html'], (req, res) => {
-    res.render('breakfastmovie');
-  });
+  server.get(
+    ["/breakfast-movie", "/breakfastmovie", "/breakfastmovie.html"],
+    (req, res) => {
+      res.render("breakfastmovie");
+    },
+  );
 
   server.get("/movies", async (req, res) => {
     const movies = await api.getAllMovies();
-    res.render('movielist', {list: movies});
+    res.render("movielist", { list: movies });
   });
 
   server.get("/movies/:movieID", async (req, res) => {
     const movieID = req.params.movieID;
     const oneMovie = await api.getOneMovie(movieID);
-    res.render('onemovie', { 
+    res.render("onemovie", {
       movietitle: oneMovie.title,
       movieintro: oneMovie.intro,
-      movieimage: oneMovie.image.url
+      movieimage: oneMovie.image.url,
     });
   });
   // Serving script static
-  server.use("/src", express.static("../src"));
+  server.use("/src", express.static("./src"));
   // Serving sub-scripts static
-  server.use("/scripts", express.static("../scripts"));
+  server.use("/scripts", express.static("./scripts"));
   // Serving mock-data static
-  server.use("/mockup_Data", express.static("../mockup_Data"));
+  server.use("/mockup_Data", express.static("./mockup_Data"));
   // Serving css static
   server.use("/css", express.static("./css"));
   // Serving asset static
-  server.use("/assets", express.static("../assets"));
+  server.use("/assets", express.static("./assets"));
 
   return server;
 }
