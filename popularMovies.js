159523c105ea6232b3c6fd66a24834ac198c9dc2
elinmolindr <elinmolinder1@gmail.com>
2026-01-30 13:52:51 +0100

    Add popular movies logic and tests

---
+++ popularMovies.js
@@ -0,0 +1,16 @@
+export async function getPopularMovies(api) {
+  const movies = await api.getPopularMovies();
+
+  //Filter out movies older then 30 days
+  const THIRTY_DAYS_IN = 30 * 24 * 60 * 60 * 1000;
+  const now = Date.now();
+
+  const recentMovies = movies.filter (movie => {
+    const reviewTime = new Date (movie.reviewDate).getTime();
+    return now - reviewTime<= THIRTY_DAYS_IN;
+  });
+
+  //Sort movies by rating and returns the top 5
+  return recentMovies
+  .sort((a, b) => b.rating - a.rating) .slice(0, 5);
+}
