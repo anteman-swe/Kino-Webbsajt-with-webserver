c3faa70b8e4bcb31fd13f948f897c1c4c5ff8f63
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-03-06 13:34:34 +0100

    Style login modal with Bootstrap classes

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ initMemberButtons();
 
 import { toggleLogin } from "/scripts/login.js";
 toggleLogin();
-import { toggleRegister } from "/scripts/register.js";
-toggleRegister();
+/*import { toggleRegister } from "/scripts/register.js";
+toggleRegister();*/
 import { toggleMenu } from "/scripts/menu.js";
 toggleMenu();
 import { closeNotice } from "/scripts/notice.js";
